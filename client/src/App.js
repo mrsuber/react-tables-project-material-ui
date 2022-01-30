71f6b9b3338d9e34a65b7beb96fbb85ee95cf91f
mohamad <msiysinyuy@gmail.com>
2022-01-30 21:52:39 +0100

    the basic data display set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,73 @@
+import {useState,forwardRef} from 'react'
+import MaterialTable from 'material-table'
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
+
+
+
+    const columns = [
+      {title:"Name",field:"name"},
+      {title:"Email",field:"email"},
+      {title:"Phone Number",field:"phone"},
+      {title:"Age",field:"age"},
+      {title:"Gender",field:"gender"},
+      {title:"City",field:"city"},
+    ]
+
+
+let data = [
+  {    name:"Mohamad", email:"msiysinyuy@gmail.com", phone:"653255547", age:25, gender:"M", city:"Buea" },
+  {    name:"musa", email:"nyuy@gmail.com", phone:"653255547", age:11, gender:"M", city:"Buea" },
+  {    name:"bib", email:"msiy@gmail.com", phone:"5446465", age:5, gender:"F", city:"Buea" },
+  {    name:"John", email:"hoihs@gmail.com", phone:"364764356", age:26, gender:"M", city:"Buea" },
+  {    name:"Haba", email:"kjha@gmail.com", phone:"645646364", age:30, gender:"F", city:"Buea" },
+  {    name:"Biitan", email:"iw@gmail.com", phone:"52468864", age:20, gender:"M", city:"Buea" },
+]
+
+const title = "Student Information"
 
 function App() {
+  const [tableData,setTableData]=useState(data)
+
+
+
   return (
+
     <div className="App">
-      This is clean app
+      <MaterialTable  icons={tableIcons} columns={columns} data={tableData} title={title}/>
     </div>
   );
 }
